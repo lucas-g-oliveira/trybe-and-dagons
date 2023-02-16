ccf554e61e46bee471147089a2ff912a234771c1
Lucas G <lucasg.devmail93@gmail.com>
2023-02-16 20:54:02 -0300

    fix: resolved eslint insue

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,4 +1,8 @@
 import Archetype from './Archetype';
+import Mage from './Mage';
+import Necromancer from './Necromancer';
+import Warrior from './Warrior';
+import Range from './Range';
 
 export default Archetype;
-// exports = {};
+exports = { Mage, Necromancer, Warrior, Range };
